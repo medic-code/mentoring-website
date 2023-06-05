3ca2814d5eacbe26d883e447599bcc8d7596b380
Aaron Smith <u40as7@gmail.com>
2023-06-05 12:26:39 +0100

    fix responsive header at large screen

--- src/core_comp/header/header.styles.ts
+++ src/core_comp/header/header.styles.ts
@@ -7,10 +7,10 @@ export const HeaderWrapper = styled.header`
 
 export const StyledLogo = styled.div`
     grid-column: 1;
-    padding: 32px 0px 0px 128px;
+    padding: 32px 0px 0px 64px;
     @media (max-width: 1500px) {
-    grid-column: 2;
-    padding: 16px 0px 0px 16px;
+        grid-column: 2;
+        padding: 16px 0px 0px 16px;
 }
 `
 

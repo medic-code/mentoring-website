c74570141d94849b7711bde42fb3574307f5fd55
Aaron Smith <u40as7@gmail.com>
2023-06-05 12:32:46 +0100

    fix header responsive

--- src/core_comp/header/header.styles.ts
+++ src/core_comp/header/header.styles.ts
@@ -7,8 +7,8 @@ export const HeaderWrapper = styled.header`
 
 export const StyledLogo = styled.div`
     grid-column: 1;
-    padding: 32px 0px 0px 64px;
-    @media (max-width: 1500px) {
+    padding: 32px 0px 0px 32px;
+    @media (max-width: 1400px) {
         grid-column: 2;
         padding: 16px 0px 0px 16px;
 }
